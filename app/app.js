da2a5613c9f422ba773a4258d0e4b0317d97292b
petardev101 <workdev1989@gmail.com>
2017-09-19 15:57:11 -0500

    ng-init displaySorted

--- app/app.js
+++ app/app.js
@@ -1,38 +1,38 @@
 angular
   .module('myApp', [])
-  .controller('ShapesController', function($interval, $window) {
+  .controller('ShapesController', function ($interval, $window) {
   //variable global to controller
   var vm = this;
   //empty array to push all 100 shapes to
   vm.shapesToDisplay = [];
 
   //function to create a circle object that can be created with any desired radius
-  function circleObject(radius) {
+  function circleObject (radius) {
     //set local type property
     this.type = "circle";
     this.radius = radius;
     //has a ‘getArea’ method that returns the area of the circle
-    this.getArea = function() {
+    this.getArea = function () {
       return Math.PI * this.radius * this.radius;
     };//end getArea method
     //has a 'toString' method that returns the following: Circle: Radius = X, Area = Y, (where X and Y are the radius and the area of the circle)
-    this.toString = function() {
+    this.toString = function () {
       return "Circle: Radius = " + this.radius + ", Area = " + vm.getArea();
     };//end toString method
     // console.log(this);//logs out instance of circle object with varying radius and getArea()/toString() methods
   };//end circleObject
 
   //function to create a square object that can be created with any length of sides
-  function squareObject(sideLength) {
+  function squareObject (sideLength) {
     //set local type property
     this.type = "square";
     this.sideLength = sideLength;
     //has a 'getArea' method that returns the area of the square and
-    this.getArea = function() {
+    this.getArea = function () {
       return this.sideLength * this.sideLength;
     };//end getArea method
     //has a 'toString' method that returns the following: Square: Size = X, Area = Y, (where X and Y are the side length and area of the square) 
-    this.toString = function() {
+    this.toString = function () {
       return "Square: Size = " + this.sideLength + ", Area = " + vm.getArea();
     };//end toString method
     // console.log(this);//logs out square object with varying side length and getArea()/toString() methods
@@ -40,9 +40,9 @@ angular
 
   //method that accepts array of Circles and Squares, and returns the array
   //sorted by area, in descending order.
-  function sortingFunc(circSqArray) {
+  function sortingFunc (circSqArray) {
     //method to compare areas for sorting
-    function compareAreas(a, b) {
+    function compareAreas (a, b) {
       return b.getArea() - a.getArea();
     };
     //sort through compared areas and return new array
@@ -51,7 +51,7 @@ angular
 
   //method that generates array of 50 squares of varying side length between 1 and 100,
   //and 50 circles of varying diameter between 1 and 100. The sizes of each shape should be randomly generated.
-  function generateFunc(total) {
+  function generateFunc (total) {
     //empty shapes array to push randomly generated sizes
     var shapesArray = [];
     for(i = 0; i < total; i++) {
@@ -79,6 +79,11 @@ angular
         vm.shapesToDisplay.push(sortedShapes[i]);
       }
     }
+    // $window.location.reload()
   };//end vm.displaySorted
+
+  // vm.timer = function () {
+  //   return $interval(vm.displaySorted, 500)
+  // }//end vm.timer
 
 }); //end ShapesController
